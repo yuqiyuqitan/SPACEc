64d9d797f5f23a0714c83980a890436852e19439
Martin Becker <1241516+mgbckr@users.noreply.github.com>
2023-12-03 01:25:37 +0100

    Janky fix for undeletable Mesmer_model

--- Docker/tutorial_init.sh
+++ Docker/tutorial_init.sh
@@ -2,6 +2,13 @@ echo
 echo "Starting SpacCodEx environment"
 echo "=============================="
 
+# TODO: tryin to fix issue with extracted files keeing ids from inside the archive
+#echo "Setup for user: ${RUN_UID}"
+#useradd --uid ${RUN_UID} spacodex
+#mkdir /home/spacodex
+#chown -R spacodex:spacodex /home/spacodex
+#chown -R ${RUN_ID}:${RUN_ID} /workspace
+
 echo "Checking tutorial materials."
 cd /workspace
 
@@ -18,6 +25,18 @@ if [ ! -d "/workspace/data" ] && [ ! -d "/workspace/notebooks" ]; then
 		wget -O materials.zip "https://www.dropbox.com/scl/fo/qxgrxq6y7y2jlrwhotabd/h?rlkey=3rp5cjkkrexnfxkqbtgr7l3h6&dl=1"
 		unzip materials.zip
 		rm materials.zip
+		echo
+		echo "Downloading model:"
+		cd notebooks
+		mkdir models
+		cd models
+		mkdir Mesmer_model
+		cd Mesmer_model
+		wget -O model.tar.gz "https://deepcell-data.s3-us-west-1.amazonaws.com/saved-models/MultiplexSegmentation-9.tar.gz"
+		tar -xzvf model.tar.gz
+		rm model.tar.gz
+		chown -R ${UID}:${UID} .
+		cd ../../..
 	fi
 else
 	echo "Data and notebooks folders for the tutorial are present. Skipping download."
