c05046c86998fa78a40a6e07c55c430c504b43cd
Tim Kempchen <80586406+TKempchen@users.noreply.github.com>
2024-05-22 14:12:58 +0200

    Update segmentation

--- src/spacec/helperfunctions/__init__.py
+++ src/spacec/helperfunctions/__init__.py
@@ -1,4 +1,5 @@
 from ._general import make_anndata
+from ._general import check_for_gpu
 from ._qptiff_converter import downscale_tissue
 
-__all__ = ["make_anndata", "downscale_tissue"]
+__all__ = ["make_anndata", "downscale_tissue", "check_for_gpu"]
