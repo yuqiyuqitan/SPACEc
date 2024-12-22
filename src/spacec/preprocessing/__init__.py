8424152119c556f323d8e5d167308ca2ab945e1e
Tim Kempchen <80586406+TKempchen@users.noreply.github.com>
2024-12-22 19:44:57 +0100

    Add Spillover correction to SPACEc based on CellSeg compensation
    - This feature is yet untested
    - This feature still needs a python test function
    - use at own risk

--- src/spacec/preprocessing/__init__.py
+++ src/spacec/preprocessing/__init__.py
@@ -1,8 +1,9 @@
-from ._general import filter_data, format, read_segdf, remove_noise
+from ._general import filter_data, format, read_segdf, remove_noise, compensate_cell_matrix
 
 __all__ = [
     "read_segdf",
     "filter_data",
     "format",
     "remove_noise",
+    "compensate_cell_matrix"
 ]
