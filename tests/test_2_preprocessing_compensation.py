9cc1e16ca68b09ee0181dc179d964da942eda0d7
Martin Becker <mgbckr@Mac.fritz.box>
2025-01-03 16:31:31 +0100

    Tox for macos build

---
+++ tests/test_2_preprocessing_compensation.py
@@ -0,0 +1,12 @@
+import pathlib
+from tempfile import TemporaryDirectory
+
+TEST_DIR = pathlib.Path(__file__).parent
+
+
+def test_2_preprocessing_compensation():
+    print("Testing compensation")
+    pass
+
+if __name__ == "__main__":
+    test_2_preprocessing_compensation()
