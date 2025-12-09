4cf600bad3fcff376b2db0382306d7eace9a15e6
Tim Kempchen <80586406+TKempchen@users.noreply.github.com>
2025-12-09 20:10:29 +0100

    Rename import for cellpose models
    Fixed missing alias that lead to failing segmentation

--- src/spacec/tools/_segmentation.py
+++ src/spacec/tools/_segmentation.py
@@ -14,7 +14,7 @@ import skimage
 import skimage.io
 import tensorflow as tf
 import tifffile
-from cellpose import models  # Renamed import
+from cellpose import models as cellpose_models # fixed
 from cellpose import io
 from deepcell.applications import Mesmer
 from deepcell.utils.plot_utils import create_rgb_image, make_outline_overlay
